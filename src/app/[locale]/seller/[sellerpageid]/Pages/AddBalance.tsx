d275a246956e6d392dfe93ae04c5d34adca6b8f9
codespacefirst <codespace.first@gmail.com>
2025-08-03 21:27:57 +0000

    .

--- src/app/[locale]/seller/[sellerpageid]/Pages/AddBalance.tsx
+++ src/app/[locale]/seller/[sellerpageid]/Pages/AddBalance.tsx
@@ -2,6 +2,31 @@ import React from 'react'
 
 export default function AddBalance() {
   return (
-    <section>AddBalance</section>
+    <section
+        className='w-full p-6 flex flex-col items-center'
+    >
+        <div
+            className='w-full p-4 rounded-2xl 
+                bg-white border border-gray-200'
+        >
+            <span>
+                <h1
+                    className='text-lg font-semibold'
+                    >
+                    My balance
+                </h1>
+                <p
+                    className='text-gray-500'
+                >
+                    View and manage your balance purchases transactions.
+                </p>
+            </span>
+
+            <div
+                className='w-full min-h-10 border border-orange-300 
+                    p-2 rounded-2xl bg-orange-600/10'
+            ></div>
+        </div>
+    </section>
   )
 }
