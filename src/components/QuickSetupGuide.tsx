a63e3fafc3fe3c2f9bd4bffd695059571b281565
codespacefirst <codespace.first@gmail.com>
2025-08-03 14:15:40 +0000

    .

--- src/components/QuickSetupGuide.tsx
+++ src/components/QuickSetupGuide.tsx
@@ -1,66 +1,125 @@
 "use client";
 import Image from 'next/image'
 import Link from 'next/link';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function QuickSetupGuide() {
+    const [percent, setPercent] = useState(25);
+    const [animatedWidth, setAnimatedWidth] = useState("0%");
+    const [displayedPercent, setDisplayedPercent] = useState(0);
+
+    // For smooth animation bar
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+        setAnimatedWidth(`${percent}%`);
+        }, 50);
+        return () => clearTimeout(timeout);
+    }, [percent]);
+
+    // to change the number step by step and smooth
+    useEffect(() => {
+        const duration = 1000;
+        const startTime = performance.now();
+
+        const animate = (currentTime: number) => {
+        const elapsed = currentTime - startTime;
+        const progress = Math.min(elapsed / duration, 1);
+        const currentValue = Math.round(progress * percent);
+        setDisplayedPercent(currentValue);
+
+        if (progress < 1) {
+            requestAnimationFrame(animate);
+        }
+        };
+
+        requestAnimationFrame(animate);
+    }, [percent]);
     return (
-    <section
-        className='relative w-full p-4 min-h-40 rounded-2xl 
-            bg-white shadow'
-    >
-        <h1
-            className='text-lg font-semibold text-gray-900'
+        <section
+            className='relative w-full p-4 min-h-40 rounded-2xl 
+                bg-white shadow'
         >
-            Quick setup guide
-        </h1>
-        <Image
-            src="/Pattern-1.jpg"
-            alt=''
-            fill
-            className='object-cover scale-x-[-1] opacity-10 group-hover:scale-140 transition-all duration-400'
-            loading='lazy'
-        />
-        <div className='w-full flex flex-shrink-0 items-center justify-between gap-2'>
-            {Array(3).fill(0).map((_, idx) => {
-                return (
+
+            <h1
+                className='text-lg font-semibold text-gray-900'
+            >
+                Quick setup guide
+            </h1>
+
+            <div
+                className='w-full flex items-center gap-2 py-3'
+            >
+                <div
+                    className='w-full'
+                >
                     <div
-                        key={idx}
-                        className='group relative bg-white w-1/3 h-70 rounded-2xl 
-                            overflow-hidden border border-gray-200 shadow
-                            hover:shadow-lg transition-shadow duration-300'
+                        className='rounded-full border border-blue-200 bg-blue-100'
                     >
-                        <Image
-                            src="/Grid-Pattern.jpg"
-                            alt=''
-                            fill
-                            className='object-cover opacity-10 group-hover:scale-140 transition-all duration-400'
-                            loading='lazy'
-                        />
-                        <div
-                            className='absolute bottom-0 left-0 z-20 p-4
-                                min-h-50 w-full flex flex-col justify-end
-                                space-y-2 bg-gradient-to-t from-white via-white'
-                        >
-                            <h1>Add your first product</h1>
-                            <span>
-                                <p className='text-sm text-gray-500'>
-                                    Start by adding a product and a few key details. Not ready?
-                                </p>
-                                <Link href="/seller" className='text-sm text-blue-600'>
-                                    Start with a sample product
-                                </Link>
-                            </span>
-                            <button
-                                className='primary-button rounded-xl py-1 px-4 w-max text-sm cursor-pointer'
-                            >
-                                Add product
-                            </button>
-                        </div>
+                        <span 
+                            className="relative flex h-2 rounded-full 
+                            bg-blue-600 transition-all 
+                            duration-1000 ease-out 
+                            after:absolute 
+                            after:right-0 
+                            after:-top-1 
+                            after:w-4 
+                            after:shadow 
+                            after:h-4 
+                            after:rounded-r-full
+                            after:bg-blue-600"
+                            style={{ width: animatedWidth }}
+                        ></span> 
                     </div>
-                )
-            })}
-        </div>
-    </section>
+                </div>
+                <p className='text-gray-500'>{displayedPercent}%</p>
+            </div>
+
+            <Image
+                src="/Pattern-1.jpg"
+                alt=''
+                fill
+                className='object-cover scale-x-[-1] opacity-10 group-hover:scale-140 transition-all duration-400'
+                loading='lazy'
+            />
+            <div className='w-full flex flex-shrink-0 items-center justify-between gap-2'>
+                {Array(3).fill(0).map((_, idx) => {
+                    return (
+                        <div
+                            key={idx}
+                            className='group relative bg-white w-1/3 h-70 rounded-2xl 
+                                overflow-hidden border border-gray-200 shadow shadow-gray-100'
+                        >
+                            <Image
+                                src="/Grid-Pattern.jpg"
+                                alt=''
+                                fill
+                                className='object-cover opacity-10 group-hover:scale-140 transition-all duration-400'
+                                loading='lazy'
+                            />
+                            <div
+                                className='absolute bottom-0 left-0 z-20 p-4
+                                    min-h-50 w-full flex flex-col justify-end
+                                    space-y-2 bg-gradient-to-t from-white via-white'
+                            >
+                                <h1>Add your first product</h1>
+                                <span>
+                                    <p className='text-sm text-gray-500'>
+                                        Start by adding a product and a few key details. Not ready?
+                                    </p>
+                                    <Link href="/seller" className='text-sm text-blue-600'>
+                                        Start with a sample product
+                                    </Link>
+                                </span>
+                                <button
+                                    className='primary-button rounded-xl py-1 px-4 w-max text-sm cursor-pointer'
+                                >
+                                    Add product
+                                </button>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </section>
   )
 }
