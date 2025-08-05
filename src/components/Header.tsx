cc239296f2e8af81f1c887e99d4246e8937d92c9
codespacefirst <codespace.first@gmail.com>
2025-08-05 15:47:16 +0000

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import { SwitchLanguage } from "./SwitchLanguage";
 import { getTranslations } from "next-intl/server";
 import { HeaderMenu } from "./HeaderMenu";
 import GlobalLogo from "./GlobalLogo";
+import { Heart, Search, ShoppingCart } from "lucide-react";
 
 export async function Header() {
   const t = await getTranslations("header");
@@ -30,53 +31,72 @@ export async function Header() {
   ];
 
   return (
-    <section 
-      dir="ltr"
-      className="sticky top-0 w-full py-3 lg:px-10 md:px-10 px-6 flex
-        items-center justify-between z-50 backdrop-blur-[2px] bg-white/80">
-      <GlobalLogo />
-
-      <ul 
-        className="hidden md:hidden lg:flex items-center 
-          gap-3">
-        {NaviGation_Links.map((link, idx) => (
-          <Link
-            key={idx}
-            href={link.href}
-            className="mx-2 cursor-pointer text-blue-600
-                hover:scale-105 transition-transform duration-200"
-          >
-            {link.name}
-          </Link>
-        ))}
-      </ul>
-
-      <div 
-        className="flex items-center gap-2">
+    <section
+      className="py-2 px-6 lg:px-20 md:px-20"
+    >
+      <div
+        className="w-full flex items-center justify-between gap-6"
+      >
+        <h1 className="uppercase text-3xl text-teal-600 font-cinzel font-bold">
+          Tijarex<span className="lowercase font-normal text-lg">.ma</span>
+        </h1>
         <div
-          className="hidden md:flex lg:flex"
+          className="w-full group border border-gray-200 rounded-full 
+           flex items-center p-1 overflow-hidden"
         >
-          <SwitchLanguage
-            CLASSNAME="flex items-center cursor-pointer 
-              border border-blue-200 text-xs py-1 px-3 
-              text-blue-600 rounded-xl gap-1"
+          <input 
+            type="text"
+            placeholder="Search for everything"
+            className="grow py-2 px-4 border-none outline-none"
           />
+          <span
+            className="group p-2.5 rounded-full cursor-pointer
+              bg-teal-600 text-white"
+          >
+            <Search 
+              size={20} 
+              className="group-hover:rotate-45 group-hover:scale-105 
+                transition-all duration-300"
+            />
+          </span>
         </div>
-        <Link
-          href="/auth/register" 
-            className="lg:text-normal md:text-md text-sm 
-              py-1.5 cursor-pointer px-6 rounded-xl primary-button">
-          {t("buttons.beSeller")}
-        </Link>
-        <Link
-          href="/auth/login" 
-            className="lg:text-normal md:text-md text-sm 
-              border border-blue-200 transition-colors duration-200
-              py-1 font-bold bg-blue-50/50 hover:bg-blue-50 cursor-pointer 
-              px-6 text-blue-600 rounded-xl">
-          {t("buttons.login")}
-        </Link>
-        <HeaderMenu />
+        <div
+          className="flex items-center gap-2"
+        >
+          <button
+            className="primary-button text-nowrap py-1 px-4 rounded-lg cursor-pointer"
+          >
+            Get started
+          </button>
+          <button
+            className="text-teal-600 rounded-lg px-4 py-1 
+              font-semibold cursor-pointer hover:text-teal-500"
+          >
+            Login
+          </button>
+          <div
+            className="flex items-center gap-4"
+          >
+            {/* --- Favorite --- */}
+            <span
+              className="relative text-teal-600 hover:text-teal-500 p-1 cursor-pointer"
+            >
+              <Heart size={20} />
+              <span
+                className="absolute left-3 -top-2 px-1 flex justify-center items-center rounded-full 
+                  bg-teal-600 text-sm text-white"
+              >
+                0
+              </span>
+            </span>
+            {/* --- Shopping Cart --- */}
+            <span
+              className="text-teal-600 hover:text-teal-500 p-1 cursor-pointer"
+            >
+              <ShoppingCart size={20} />
+            </span>
+          </div>
+        </div>
       </div>
     </section>
   );
