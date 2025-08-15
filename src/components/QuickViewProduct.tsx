f915f9c137b0670250286bbc87621b965799dc1e
codespacefirst <codespace.first@gmail.com>
2025-08-15 17:31:48 +0000

    .

--- src/components/QuickViewProduct.tsx
+++ src/components/QuickViewProduct.tsx
@@ -2,7 +2,7 @@
 
 import { useQuickViewProduct } from "@/context/QuickViewProductContext";
 import { ProductType } from "@/types/product";
-import { BadgeCheck, Star } from "lucide-react";
+import { BadgeCheck, Heart, ShoppingCart, Star } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import { CalculateDiscount } from "./Functions/CalculateDiscount";
@@ -57,6 +57,8 @@ export function QuickViewProduct() {
     }, [setIsShowQuickViewProduct]);
     
     const [currentImage, setCurrentImage] = useState(0);
+    const [selectedColor, setSelectedColor] = useState("");
+
     if (!isVisible) return null;
 
     return (
@@ -72,9 +74,9 @@ export function QuickViewProduct() {
                     ${isShowQuickViewProduct ? "scale-100 opacity-100" : "scale-95 opacity-0"}`}
             >
                 <div className="w-full flex flex-shrink-0 items-start justify-between gap-2">
-                    <div className="w-[400px] space-y-3">
+                    <div className="w-[500px] space-y-3">
                         <div 
-                            className="relative w-full h-[400px] rounded-xl 
+                            className="relative w-full h-[500px] rounded-xl 
                                 overflow-hidden border border-gray-200 bg-gray-50">
                             <Image
                                 src={selectedProductDetails?.product_images[currentImage] || ""}
@@ -109,7 +111,7 @@ export function QuickViewProduct() {
                     </div>
                     <div 
                         className="w-[400px] min-h-40 border border-gray-200 
-                            p-3 rounded-xl space-y-3">
+                            p-3 rounded-xl space-y-4">
                         <div className="flex items-center gap-2">
                             <div 
                                 className="relative border border-gray-200 
@@ -137,27 +139,138 @@ export function QuickViewProduct() {
                         <h1
                             className="text-2xl font-semibold"
                         >
-                            {selectedProductDetails?.name}
+                            {selectedProductDetails?.title}
                         </h1>
-                        <span
-                            className="flex items-end gap-3"
+                        <div
+                            className="flex items-center justify-between"
                         >
-                            <ins
-                                className="no-underline text-xl text-gray-600"
+                            <span
+                                className="flex items-end gap-3"
                             >
-                                {selectedProductDetails?.sale_price} Dh
-                            </ins>
-                            <del
-                                className="text-gray-400 text-md"
-                            >
-                                {selectedProductDetails?.regular_price} Dh
-                            </del>
+                                <ins
+                                    className="no-underline text-xl text-gray-600"
+                                >
+                                    {selectedProductDetails?.sale_price} Dh
+                                </ins>
+                                <del
+                                    className="text-gray-400 text-sm"
+                                >
+                                    {selectedProductDetails?.regular_price} Dh
+                                </del>
+                            </span>
                             <p
-                                className="text-teal-600 px-3"
+                                className="text-sm uppercase text-teal-600 px-3"
                             >
                                 {CalculateDiscount(selectedProductDetails?.sale_price, selectedProductDetails?.regular_price)}% off
                             </p>
-                        </span>
+                        </div>
+                        <div
+                            className="flex items-center gap-6"
+                        >
+                            <span
+                                className="flex items-center"
+                            >
+                                {Array(5).fill(0).map((_, idx) => {
+                                    if(!selectedProductDetails?.rating) return;
+                                    return (
+                                        <Star
+                                            key={idx}
+                                            size={16}
+                                            className={`
+                                                ${idx < selectedProductDetails?.rating ? "fill-yellow-500 text-yellow-500" : "text-yellow-500"}
+                                            `}
+                                        />
+                                    )
+                                })}
+                            </span>
+                            <span
+                                className="text-gray-400"
+                            >
+                                {selectedProductDetails?.rating} ({selectedProductDetails?.reviewCount} reviews)
+                            </span>
+                        </div>
+                        <div
+                            className="space-y-1"
+                        >
+                            <h1
+                                className="text-lg"
+                            >
+                                Description
+                            </h1>
+                            <p
+                                className="text-sm text-gray-500"
+                            >
+                                {selectedProductDetails?.description}
+                            </p>
+                        </div>
+                        <div>
+                            <h1
+                                className="text-lg"
+                            >
+                                Tags
+                            </h1>
+                            <div
+                                className="flex items-center gap-3 py-3"
+                            >
+                                {selectedProductDetails?.colors.map((col, idx) => {
+                                    const selectedcol = selectedColor === "" ? selectedProductDetails?.colors[0].color : selectedColor;
+                                    return (
+                                        <span 
+                                            key={idx}
+                                            onClick={() => setSelectedColor(col.color)}
+                                            style={{ backgroundColor: col.color }}
+                                            className={`flex transition-all duration-200 
+                                                w-6 h-6 rounded-full
+                                                ${selectedcol === col.color ? "ring-2 border border-gray-200 ring-teal-600" : "cursor-pointer"}`}
+                                        />
+                                    )
+                                })}
+                            </div>
+                        </div>
+                        <div>
+                            <h1
+                                className="text-lg"
+                            >
+                                Quantity
+                            </h1>
+                            <div
+                                className="flex gap-3 items-center text-lg py-3"
+                            >
+                                <button
+                                    className="px-6 bg-gray-50 border 
+                                        border-transparent cursor-pointer 
+                                        hover:border-gray-200 rounded-lg 
+                                        transition-all duration-200 text-xl"
+                                >-</button>
+                                <span>1</span>
+                                <button
+                                    className="px-6 bg-gray-50 border 
+                                        border-transparent cursor-pointer 
+                                        hover:border-gray-200 rounded-lg 
+                                        transition-all duration-200 text-xl"
+                                >+</button>
+                            </div>
+                        </div>
+                        <div
+                            className="w-full flex items-center gap-2"
+                        >
+                            <button
+                                className="w-full flex justify-center items-center gap-1
+                                    border-b-2 border-gray-300 cursor-pointer rounded-xl py-1
+                                    bg-gradient-to-r from-teal-600 to-teal-500 text-white
+                                    hover:from-teal-500 hover:to-teal-600 transition-colors duration-200"
+                            >
+                                <ShoppingCart size={16} /> Add to Cart
+                            </button>
+                            <button
+                                className="w-full flex justify-center items-center gap-1
+                                    border-b-2 border-gray-300 rounded-xl py-1 cursor-pointer
+                                    bg-gradient-to-r from-black to-black/80 text-white
+                                    hover:from-black/80 hover:to-black transition-colors duration-200"
+                            >
+                                <Heart size={16} /> Add to Wishlist
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
