aee6b65f6d0d21ef5d0584ed70e111207cbe8715
codespacefirst <codespace.first@gmail.com>
2025-08-16 16:28:25 +0000

    .

--- src/app/[locale]/shop/page.tsx
+++ src/app/[locale]/shop/page.tsx
@@ -1,5 +1,7 @@
 import { ShopInputSearch } from '@/components/ShopInputSearch';
+import { Tag } from 'lucide-react';
 import { Metadata } from 'next';
+import Link from 'next/link';
 import React from 'react'
 
 
@@ -22,7 +24,29 @@ export default function page({ searchParams }: ShopPageProps) {
         className='w-full min-h-70 shrink-0 bg-white p-4 shadow-md 
             rounded-xl overflow-y-auto'
     >
-        <ShopInputSearch />
+        <div>
+            <ShopInputSearch />
+            <div
+                className='flex items-center gap-3 justify-center py-3'
+            >
+                {["sport", "books", "fashion", "electronics"].map((sug, idx) => {
+                    return (
+                        <Link
+                            key={idx}
+                            href={`/shop?cat=${sug.toLowerCase().replace(" ", "")}`}
+                        >
+                            <span
+                                className='text-sm bg-gray-100 ring ring-gray-200 
+                                    rounded-full px-2 flex items-center gap-1
+                                    hover:bg-gray-200'
+                            >
+                                <Tag size={12} /> {sug}
+                            </span>
+                        </Link>
+                    )
+                })}
+            </div>
+        </div>
     </section>
   )
 }
