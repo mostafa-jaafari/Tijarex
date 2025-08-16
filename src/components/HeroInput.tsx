9dc4d6b3015700185853fff9a17c14f956797fb7
codespacefirst <codespace.first@gmail.com>
2025-08-16 00:26:02 +0000

    .

--- src/components/HeroInput.tsx
+++ src/components/HeroInput.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ProductType } from "@/types/product";
 import { PackageSearch } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
@@ -15,7 +16,7 @@ export function InputHero() {
         { label: "Gifts & art collectibles", icon: PackageSearch },
     ];
 
-    const [showSuggestionsMenu, setShowSuggestionsMenu] = useState(false);
+    const [showSuggestionsMenu, setShowSuggestionsMenu] = useState(true);
     const MenuRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
@@ -28,8 +29,78 @@ export function InputHero() {
         return () => document.removeEventListener("mousedown", handleHideMenu);
     }, []);
 
+    const [searchResult, setSearchResult] = useState<ProductType[] | []>([]);
+    const [isLoadingSearch, setIsLoadingSearch] = useState(false);
+    useEffect(() => {
+        if(searchInput === ""){
+            setSearchResult([]);
+            setIsLoadingSearch(false);
+            return;
+        }
+        const controller = new AbortController();
+        const handleFetchSearch = async () => {
+            setIsLoadingSearch(true);
+            try {
+                const Res = await fetch("/api/products");
+                const { products } = await Res.json();
+                if(Array.isArray(products)){
+                    const FiltredSearch = products.filter((product: ProductType) => 
+                        product.category.includes(searchInput.toLowerCase()) || 
+                        product.owner?.name.toLowerCase().includes(searchInput.toLowerCase()) ||
+                        product.title.toLowerCase().includes(searchInput.toLowerCase())
+                );
+                    setSearchResult(FiltredSearch as ProductType[]);
+                }else{
+                    setSearchResult([]);
+                }
+            }catch (err){
+                console.log(err)
+            }finally{
+                setIsLoadingSearch(false);
+            }
+        }
+        const timeoutId = setTimeout(() => handleFetchSearch(), 300);
+        return () => {
+            clearTimeout(timeoutId);
+            controller.abort();
+        };
+    },[searchInput])
+
+function highlightMatch(text: string, query: string) {
+  if (!query) return text;
+  
+  const parts = [];
+  let lastIndex = 0;
+  
+  text.replace(new RegExp(`(${query})`, 'gi'), (match, p1, offset) => {
+    // Add text before match
+    if (offset > lastIndex) {
+      parts.push(text.slice(lastIndex, offset));
+    }
+    
+    // Add highlighted match
+    parts.push(
+      <span key={offset} className="text-yellow-500">
+        {match}
+      </span>
+    );
+    
+    lastIndex = offset + match.length;
+    return match;
+  });
+  
+  // Add remaining text
+  if (lastIndex < text.length) {
+    parts.push(text.slice(lastIndex));
+  }
+  
+  return parts.length ? parts : text;
+}
+
+
     return (
         <section ref={MenuRef} className="relative w-full max-w-[700px]">
+            {searchResult.length} / {isLoadingSearch ? "Loading..." : ""}
             <div className="w-full bg-white h-14 rounded-full overflow-hidden p-0.5 flex items-center">
                 <input
                     type="text"
@@ -53,11 +124,47 @@ export function InputHero() {
                 }`}
             >
                 <div 
-                    className="w-full bg-white rounded-xl border 
+                    className="w-full max-h-64 bg-white rounded-xl border 
                         p-3 border-gray-200 flex flex-col 
-                        overflow-hidden shadow-md"
+                        overflow-auto shadow-md"
                 >
-                    {SearchSuggestions.map((item, idx) => (
+                    {isLoadingSearch ? 
+                    (
+                        <div
+                            className="flex flex-col gap-2"
+                        >
+                            <span className="w-4/4 h-5 animate-pulse flex bg-gray-200 rounded-full"/>
+                            <span className="w-3/4 h-5 animate-pulse flex bg-gray-200 rounded-full"/>
+                            <span className="w-2/4 h-5 animate-pulse flex bg-gray-200 rounded-full"/>
+                            <span className="w-4/4 h-5 animate-pulse flex bg-gray-200 rounded-full"/>
+                            <span className="w-3/4 h-5 animate-pulse flex bg-gray-200 rounded-full"/>
+                            <span className="w-2/4 h-5 animate-pulse flex bg-gray-200 rounded-full"/>
+                        </div>
+                    )
+                    :
+                    searchResult.length !== 0 ?
+                    searchResult.map((item, idx) => (
+                        <Link
+                            key={idx}
+                            href="/"
+                            className="w-full flex items-center gap-2 py-2 
+                                px-3 text-sm hover:bg-gray-50 rounded-xl"
+                        >
+                            <PackageSearch size={16} className="text-gray-500" />{" "}
+                            {highlightMatch(item.title, searchInput)}
+                        </Link>
+                    ))
+                    :
+                    searchResult.length === 0 && searchInput !== "" ?
+                    (
+                        <div
+                            className="w-full flex justify-center py-3 text-gray-400"
+                        >
+                            Not Founded !
+                        </div>
+                    )
+                    :
+                    searchInput === "" && SearchSuggestions.map((item, idx) => (
                         <Link
                             key={idx}
                             href="/"
