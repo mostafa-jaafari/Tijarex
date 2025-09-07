57fb4a80e216b05b3eb01b0066a26c3c9bc9e9e9
stoph2003 <mostafaimrane9@gmail.com>
2025-09-07 16:57:41 +0000

    .

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,30 @@
+import Image from 'next/image'
 import React from 'react'
 
 export function HeroSection() {
   return (
-    <section>
-      Hero section
+    <section
+      className='min-h-100 px-6 bg-white flex items-center justify-between gap-3'
+    >
+      <div
+        className='relative grow h-90 overflow-hidden rounded-xl flex-shrink-0 
+          bg-red-500'
+      >
+        <Image
+          src="/HeroImage1.jpg"
+          fill
+          alt='Hero-Image-1.jpg'
+          className='object-cover object-top'
+          priority
+          quality={100}
+        />
+      </div>
+      <div
+        className='w-3/5 h-90 overflow-hidden rounded-xl flex-shrink-0 
+          bg-green-500'
+      >
+        Second
+      </div>
     </section>
   )
 }
